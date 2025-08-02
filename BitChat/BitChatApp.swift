f3c1af62667144d836bc90febf7aacc80b9133d4
Rachit Goyal <rg410345@gmail.com>
2025-08-02 20:51:37 +0530

    Initial Commit

---
+++ BitChat/BitChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitChatApp.swift
+//  BitChat
+//
+//  Created by Rachit Goyal on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct BitChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitChatTests/BitChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  BitChatTests.swift
+//  BitChatTests
+//
+//  Created by Rachit Goyal on 02/08/25.
+//
+
+import Testing
+@testable import BitChat
+
+struct BitChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
